4ca7a0b2797e5dc91913c2a9c510800a16ba138c
Jess Roque <68854057+jessdolt@users.noreply.github.com>
2024-03-05 15:20:09 +0800

    prisma init

---
+++ components/auth/register-form.tsx
@@ -0,0 +1,137 @@
+"use client"
+import { useState, useTransition } from "react"
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import CardWrapper from "./card-wrapper"
+import { RegisterSchema } from "@/schemas"
+import { Input } from "../ui/input"
+import { Button } from "../ui/button"
+import FormError from "../form-error"
+import FormSuccess from "../form-success"
+import { login } from "@/actions/login"
+
+export const RegisterForm = () => {
+  const [error, setError] = useState<string | undefined>("")
+  const [success, setSuccess] = useState<string | undefined>("")
+  const [isPending, startTransition] = useTransition()
+
+  const form = useForm<z.infer<typeof RegisterSchema>>({
+    resolver: zodResolver(RegisterSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+      name: "",
+    },
+  })
+
+  const clearFormState = () => {
+    setSuccess("")
+    setError("")
+  }
+
+  const onSubmit = (values: z.infer<typeof RegisterSchema>) => {
+    clearFormState()
+
+    startTransition(async () => {
+      try {
+        await new Promise((resolve) =>
+          setTimeout(() => {
+            resolve("zxc")
+          }, 1000)
+        )
+
+        const response = await login(values)
+
+        setError(response?.error)
+        setSuccess(response?.success)
+      } catch (e) {
+        console.log(e)
+      }
+    })
+  }
+
+  return (
+    <CardWrapper
+      headerLabel="Create an account"
+      backButtonLabel="Already have an account?"
+      backButtonHref="/auth/login"
+      showSocial
+    >
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+          <div className="space-y-4">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      type="text"
+                      placeholder="John Doe"
+                      disabled={isPending}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      type="email"
+                      placeholder="john.doe@example.com"
+                      disabled={isPending}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      type="password"
+                      placeholder="****"
+                      disabled={isPending}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <FormError message={error} />
+          <FormSuccess message={success} />
+          <Button className="w-full" type="submit" disabled={isPending}>
+            Register
+          </Button>
+        </form>
+      </Form>
+    </CardWrapper>
+  )
+}
